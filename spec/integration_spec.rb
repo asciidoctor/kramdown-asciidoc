20f5fe5d9a156258c10dd3a7ea42ac5835416ccb
Dan Allen <dan.j.allen@gmail.com>
2018-05-18 01:17:19 -0600

    fix integration tests so they actually work

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,19 +1,19 @@
 require_relative 'spec_helper'
 
-describe 'integration' do
+describe 'integration scenario' do
   FIXTURES_DIR = File.absolute_path 'fixtures', __dir__
 
   let(:doc) { Kramdown::Document.new input, Kramdown::Converter::AsciiDoc::DEFAULT_PARSER_OPTS }
 
-  context 'converts Markdown to AsciiDoc' do
-    (Dir.glob ([FIXTURES_DIR, '**', '*.md'].join '/')).each do |input_file|
-      relative_path = input_file.slice FIXTURES_DIR.length + 1, input_file.length
-      scenario_name = (relative_path.slice 0, relative_path.length - 3).tr '/', ' '
-      input_basename = File.basename input_file, '.md'
-      output_file = [(File.dirname input_file), %(#{input_basename}.adoc)].join '/'
+  (Dir.glob ([FIXTURES_DIR, '**', '*.md'].join '/')).each do |input_file|
+    relative_path = input_file.slice FIXTURES_DIR.length + 1, input_file.length
+    scenario_name = (relative_path.slice 0, relative_path.length - 3).gsub '/', '::'
+    input_basename = File.basename input_file, '.md'
+    output_file = [(File.dirname input_file), %(#{input_basename}.adoc)].join '/'
+    context %(for #{scenario_name}) do
       let(:input) { ::IO.read input_file }
       let(:expected) { (::IO.read output_file).chomp }
-      it %(when content contains #{scenario_name}) do
+      it 'converts Markdown to AsciiDoc' do
         (expect doc.to_asciidoc).to eql expected
       end
     end
