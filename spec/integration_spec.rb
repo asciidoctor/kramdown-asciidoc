d7d8ea562ae6c6585bc3028a0a450b0ddbaf0d4a
Dan Allen <dan.j.allen@gmail.com>
2018-05-21 00:03:30 -0600

    simplify integration test setup
    - switch to fixtures dir before globbing
    - compute input stem to make calculating related files easier
    - read files in binary mode with UTF-8 encoding

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,19 +6,21 @@ describe 'integration scenario' do
 
   let(:doc) { Kramdown::Document.new input, (Kramdown::Converter::AsciiDoc::DEFAULT_PARSER_OPTS.merge extra_options) }
 
-  (Dir.glob ([FIXTURES_DIR, '**', '*.md'].join '/')).each do |input_file|
-    relative_path = input_file.slice FIXTURES_DIR.length + 1, input_file.length
-    scenario_name = (relative_path.slice 0, relative_path.length - 3).gsub '/', '::'
-    input_basename = File.basename input_file, '.md'
-    options_file = [(File.dirname input_file), %(#{input_basename}.opts)].join '/'
-    options = (File.exist? options_file) ? (YAML.load_file options_file) : {}
-    output_file = [(File.dirname input_file), %(#{input_basename}.adoc)].join '/'
-    context %(for #{scenario_name}) do
-      let(:input) { ::IO.read input_file }
-      let(:extra_options) { options }
-      let(:expected) { (::IO.read output_file).chomp }
-      it 'converts Markdown to AsciiDoc' do
-        (expect doc.to_asciidoc).to eql expected
+  Dir.chdir FIXTURES_DIR do
+    (Dir.glob '**/*.md').each do |input_filename|
+      input_stem = input_filename.slice 0, input_filename.length - 3
+      scenario_name = input_stem.gsub '/', '::'
+      options_filename = %(#{input_stem}.opts)
+      options = (File.exist? options_filename) ? (YAML.load_file options_filename) : {}
+      input_filename = File.absolute_path input_filename
+      output_filename = File.absolute_path %(#{input_stem}.adoc)
+      context %(for #{scenario_name}) do
+        let(:input) { ::IO.read input_filename, open_args: ['rb:UTF-8'] }
+        let(:extra_options) { options }
+        let(:expected) { (::IO.read output_filename, open_args: ['rb:UTF-8']).chomp }
+        it 'converts Markdown to AsciiDoc' do
+          (expect doc.to_asciidoc).to eql expected
+        end
       end
     end
   end
