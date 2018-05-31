7ac4e600d9960a20fb7fcab1d8fc7587d3f356e7
Dan Allen <dan.j.allen@gmail.com>
2018-05-30 22:30:55 -0600

    patch Kramdown to prevent crash when HTML br element appears in table cell

--- lib/kramdown-asciidoc.rb
+++ lib/kramdown-asciidoc.rb
@@ -1,3 +1,13 @@
 require 'kramdown'
 require_relative 'kramdown-asciidoc/converter'
 autoload :YAML, 'yaml'
+
+# REMOVE once Kramdown > 1.16.2 is released
+class Kramdown::Parser::Html::ElementConverter
+  def convert_br el
+    el.options.clear
+    el.type = el.value.to_sym
+    el.value = ''
+    nil
+  end
+end
