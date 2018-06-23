890ac50779aaf1cc145187116c221b82430a36c2
Dan Allen <dan.j.allen@gmail.com>
2018-06-22 22:59:05 -0700

    resolves #2 add first iteration of CLI that shows usage and accepts args (PR #13)

---
+++ spec/cli_spec.rb
@@ -0,0 +1,43 @@
+require_relative 'spec_helper'
+require 'kramdown-asciidoc/cli'
+require 'stringio'
+
+describe Kramdown::AsciiDoc::Cli do
+  before :each do
+    @old_stdout, $stdout = $stdout, StringIO.new
+  end
+
+  after :each do
+    $stdout = @old_stdout
+  end
+
+  context 'flags' do
+    it 'displays version when -v flag is used' do
+      (expect Kramdown::AsciiDoc::Cli.run %w(-v)).to eql 0
+      (expect $stdout.string.chomp).to eql %(kramdoc #{Kramdown::AsciiDoc::VERSION})
+    end
+
+    it 'displays help when -h flag is used' do
+      (expect Kramdown::AsciiDoc::Cli.run %w(-h)).to eql 0
+      (expect $stdout.string.chomp).to include 'Usage: kramdoc'
+    end
+
+    it 'writes output to stdout when -o flag equals -' do
+      source_file = File.absolute_path 'scenarios/p/single-line.md', __dir__
+      (expect Kramdown::AsciiDoc::Cli.run %W(-o - #{source_file})).to eql 0
+      (expect $stdout.string.chomp).to include 'A paragraph that consists of a single line.'
+    end
+
+    it 'reads input from stdin when argument is -' do
+      old_stdin, $stdin = $stdin, StringIO.new
+      begin
+        $stdin.puts '- list item'
+        $stdin.rewind
+        (expect Kramdown::AsciiDoc::Cli.run %W(-o - -)).to eql 0
+        (expect $stdout.string.chomp).to eql '* list item'
+      ensure
+        $stdin = old_stdin
+      end
+    end
+  end
+end
