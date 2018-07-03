9f22a0b568925e517ed8c1d4ff0737ed591f258f
Dan Allen <dan.j.allen@gmail.com>
2018-07-03 00:02:19 -0700

    add support for Ruby 2.3 (PR #18)

--- lib/kramdown-asciidoc.rb
+++ lib/kramdown-asciidoc.rb
@@ -1,4 +1,5 @@
 require 'kramdown'
+require_relative 'kramdown-asciidoc/core_ext/regexp/is_match'
 require_relative 'kramdown-asciidoc/converter'
 require_relative 'kramdown-asciidoc/writer'
 autoload :YAML, 'yaml'
