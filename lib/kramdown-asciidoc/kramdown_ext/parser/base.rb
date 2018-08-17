1b397e6e68cd973e8c4417b2081dd457755a833b
Dan Allen <dan.j.allen@gmail.com>
2018-08-17 01:51:44 -0600

    add internal method to extract raw text consistently
    - take over responsibility to extract raw text
    - preserve character references
    - use same logic to extract raw text for any input

--- lib/kramdown-asciidoc/kramdown_ext/parser/base.rb
+++ lib/kramdown-asciidoc/kramdown_ext/parser/base.rb
@@ -1,7 +1,7 @@
 class Kramdown::Parser::Base
   # Overload parse to force value of :auto_ids option to false; handled by converter instead
   def self.parse source, options
-    (parser = new source, (options.merge auto_ids: false)).parse
+    (parser = new source, (options.merge auto_ids: false, auto_id_stripping: false)).parse
     [parser.root, parser.warnings]
   end
 end
