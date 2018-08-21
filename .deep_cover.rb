3dcd5426de40602c7a8a1d079c1680379ba899ce
Dan Allen <dan.j.allen@gmail.com>
2018-08-20 23:59:05 -0600

    allow coverage reporter to be changed using environment variable
    - set reporter to :istanbul when DEEP_COVER_REPORTER=istanbul is set
    - document how to change reporter

--- .deep_cover.rb
+++ .deep_cover.rb
@@ -2,5 +2,5 @@ DeepCover.configure do
   output 'coverage/report-deep-cover'
   paths %w(./lib)
   reporter :text if ENV['CI']
-  #reporter :istanbul
+  reporter :istanbul if ENV['DEEP_COVER_REPORTER'] == 'istanbul'
 end
